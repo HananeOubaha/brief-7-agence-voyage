c6f28e0eb9cee550949186ebdd462d42148526f8
Hanane Oubaha <Hananeouba03@gmail.com>
2024-12-10 11:43:18 +0100

    j'ai crée le 2 eme tableau pour activite

--- script.sql
+++ script.sql
@@ -3,11 +3,17 @@ create database vyg ;
 USE vyg;
 --  création du tableau pour client 
 CREATE TABLE client (
-    id_client INT primary key NOT NULL ,
+    id_client INT primary key NOT NULL AUTO_INCREMENT ,
     nom VARCHAR(100),
     prenom VARCHAR(100),
     email VARCHAR(150)UNIQUE ,
     telephone VARCHAR(150),
     adresse TEXT ,
     date_naissance DATE
+);
+--  création du tableau pour activité
+CREATE TABLE activite (
+    id_reservation INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    date_reservation TIMESTAMP,
+    statut ENUM('En attente', 'Confirmée','Annulée')
 );
